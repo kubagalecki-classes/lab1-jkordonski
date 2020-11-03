0ab15fcfebbc49dd82094d2744b201bb6e8326d3
jkordonski <73549154+jkordonski@users.noreply.github.com>
2020-11-03 22:18:06 +0100

    Update Wektor2D.hpp
    test

--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -1,4 +1,36 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+   {
+public:
+    double x;
+    double y;
+
+    void   setX(double a) { x = a; }
+    double getX(double x) { return x; }
+    void   setY(double b) { y = b; }
+    double getY(double y) { return y; }
+
+    Wektor2D()
+    {
+        setX(0);
+        setY(0);
+    }
+    Wektor2D(double a, double b)
+    {
+        setX(a);
+        setY(b);
+    }
+    Wektor2D operator+(Wektor2D param)
+    {
+        Wektor2D temp;
+        temp.x = x + param.x;
+        temp.y = y + param.y;
+
+        return (temp);
+    }
+
+    friend double operator*(const Wektor2D v1, const Wektor2D v2)
+    {
+        return v1.x * v2.x + v1.y * v2.y;
+    }
 };
